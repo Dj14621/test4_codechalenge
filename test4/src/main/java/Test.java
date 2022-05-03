e6fcfe411209889ab6644879ac76a3810e33df16
DJ Kraken <haradj.gmail@gmail.com>
2022-05-03 16:46:21 -0400

    added code

---
+++ test4/src/main/java/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+    public static void main(String[] args) {
+
+
+        int n = 5;
+        String str = "";
+
+        for (int i = n; i >= 0; i--) {
+            str += i;
+        }
+        System.out.println("str");
+    }
+}
